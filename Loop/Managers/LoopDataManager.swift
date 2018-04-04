b2bd1f5bb916b05b52b8d636a831f5fd540fb3bb
elnjensen <elnjensen@gmail.com>
2018-04-03 20:10:35 -0400

    Carb absorption times

--- Loop/Managers/LoopDataManager.swift
+++ Loop/Managers/LoopDataManager.swift
@@ -68,8 +68,8 @@ final class LoopDataManager {
         carbStore = CarbStore(
             healthStore: healthStore,
             defaultAbsorptionTimes: (
-                fast: TimeInterval(hours: 2),
-                medium: TimeInterval(hours: 3),
+                fast: TimeInterval(hours: 1),
+                medium: TimeInterval(hours: 2.5),
                 slow: TimeInterval(hours: 4)
             ),
             carbRatioSchedule: carbRatioSchedule,
